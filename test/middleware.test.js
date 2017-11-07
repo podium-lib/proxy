aa9dde88b0ccf7ce5cdbc42405f70d76c87f03ac
ivaosthu <ivar.conradi.osthus@finn.no>
2017-11-07 08:57:11 +0100

    Remove http-proxy-middleware due to memory-leaks
    After testing this middleware for a few days we
    discovered a small memory-leak.

--- test/middleware.test.js
+++ test/middleware.test.js
@@ -5,7 +5,6 @@ const express = require('express');
 const PodletClient = require('@podium/podlet-client');
 const { browserMiddleware } = require('@podium/context');
 const supertest = require('supertest');
-const nock = require('nock');
 const configDefs = require('../config');
 
 const mockProxyImplementation = jest.fn();
@@ -86,47 +85,4 @@ test('should not proxy calls for unknown podlets', async () => {
 
     expect(errors).toHaveLength(0);
     expect(mockProxyImplementation).not.toHaveBeenCalled();
-});
-
-test('should not use custom proxy for last-searches', async () => {
-    const scope = nock('http://podlet-server', {
-        reqheaders: {
-            host: 'podlet-server',
-        },
-    })
-        .get('/user-searches/public/last-searches')
-        .reply(200);
-
-    const app = express();
-    const id = 'test-crash-idiots';
-
-    const client = new PodletClient();
-    client.register({ uri: `http://${id}`, name: 'lastSearches' });
-
-    const config = getConfig({ APP_NAME: 'supah-server-2' });
-    const resourceProxy = new ResourceProxy(client, config);
-
-    app.use((req, res, next) => {
-        req.unleash = { 'podium.useExpressProxy': true };
-
-        next();
-    });
-    app.use(browserMiddleware(config));
-    app.use(resourceProxy.middleware());
-
-    const errors = [];
-    app.use((err, req, res, next) => {
-        errors.push(err);
-        next();
-    });
-
-    // request the resource
-    const res = await supertest(app).get(
-        '/podium-resource/userSearches/last-searches'
-    );
-
-    expect(errors).toHaveLength(0);
-    expect(res.status).toBe(200);
-    expect(mockProxyImplementation).not.toHaveBeenCalled();
-    expect(scope.isDone()).toBe(true);
 });
