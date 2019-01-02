17f718a189a08c1c3bcd61041a8dd0527c250066
Richard Walker <digitalsadhu@gmail.com>
2019-01-02 13:46:55 +0100

    add metrics collection timers

--- __tests__/proxying.js
+++ __tests__/proxying.js
@@ -3,7 +3,26 @@
 const express = require('express');
 const http = require('http');
 const { URL } = require('url');
+const { Writable } = require('readable-stream');
 const Proxy = require('../');
+
+const destObjectStream = done => {
+    const arr = [];
+
+    const dStream = new Writable({
+        objectMode: true,
+        write(chunk, encoding, callback) {
+            arr.push(chunk);
+            callback();
+        },
+    });
+
+    dStream.on('finish', () => {
+        done(arr);
+    });
+
+    return dStream;
+};
 
 /**
  * Destination server utility
@@ -304,4 +323,49 @@ test('Proxying() - POST request - should proxy query params to "{destination}/so
 
     await proxy.close();
     await server.close();
+});
+
+test('Proxying() - metrics collection', async done => {
+    const server = new DestinationServer();
+    const serverAddr = await server.listen();
+
+    const proxy = new ProxyServer([
+        {
+            name: 'foo',
+            proxy: {
+                a: '/foo',
+            },
+            version: '1.0.0',
+            content: '/',
+        },
+        {
+            name: 'bar',
+            proxy: {
+                a: `${serverAddr}/some/path`,
+            },
+            version: '1.0.0',
+            content: '/',
+        },
+    ]);
+
+    proxy.proxy.metrics.pipe(
+        destObjectStream(arr => {
+            expect(arr).toHaveLength(2);
+            expect(arr[0].meta.podlet).toBe('foo');
+            expect(arr[0].meta.proxy).toBe('a');
+            expect(arr[1].meta.podlet).toBe('bar');
+            expect(arr[1].meta.proxy).toBe('a');
+            done();
+        }),
+    );
+
+    await proxy.listen();
+
+    await proxy.get('/layout/proxy/foo/a');
+    await proxy.get('/layout/proxy/bar/a');
+
+    await proxy.close();
+    await server.close();
+
+    proxy.proxy.metrics.push(null);
 });
