f408af7f97928b2599078ad827f72e740311ca27
Trygve Lie <post@trygve-lie.com>
2019-05-27 22:17:58 +0200

    Use @podium/test-utils for destination stream

--- __tests__/proxying.js
+++ __tests__/proxying.js
@@ -1,28 +1,11 @@
 'use strict';
 
-const http = require('http');
-const { URL } = require('url');
-const { Writable } = require('readable-stream');
+const { destinationObjectStream } = require('@podium/test-utils');
 const { HttpIncoming } = require('@podium/utils');
+const { URL } = require('url');
+const http = require('http');
+
 const Proxy = require('../');
-
-const destObjectStream = done => {
-    const arr = [];
-
-    const dStream = new Writable({
-        objectMode: true,
-        write(chunk, encoding, callback) {
-            arr.push(chunk);
-            callback();
-        },
-    });
-
-    dStream.on('finish', () => {
-        done(arr);
-    });
-
-    return dStream;
-};
 
 /**
  * Destination server utility
@@ -387,7 +370,7 @@ test('Proxying() - metrics collection', async done => {
     ]);
 
     proxy.proxy.metrics.pipe(
-        destObjectStream(arr => {
+        destinationObjectStream(arr => {
             expect(arr).toHaveLength(2);
             /*
             TODO: Commented out due to the way we get this info is wrong
